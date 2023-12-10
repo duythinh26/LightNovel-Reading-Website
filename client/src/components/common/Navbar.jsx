d8af15c29cc75baf852492c9060ff37642aabe22
duythinh26 <pduythinh79@gmail.com>
2023-12-10 16:34:43 +0700

    setup client

---
+++ client/src/components/common/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="container">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Navbar

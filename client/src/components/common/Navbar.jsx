db586e86bb56bb08aa7c1e974b15e60370d1e60c
duythinh26 <pduythinh79@gmail.com>
2024-01-08 17:07:40 +0700

    add more features for navbar

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom"
 import logo from "../../assets/images/logo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     const logoUrl = '../images/logo.png'
     return (
         <div className="navbar">
             <div className="container">
-                {/* Logo part */}
+                {/* Logo */}
                 <div className="navbar-logo-wrapper">
                     <a href="/" 
                         className="navbar-logo"
@@ -14,8 +16,21 @@ const Navbar = () => {
                             <img src={logo} alt="" />
                     </a>
                 </div>
+
+                {/* Login block */}
+                <div className="navbar-user">
+                    <a href="/login" className="login">Đăng nhập</a>
+                    <div className="guest-menu">
+                        <div className="icon">
+                            {/* <span className="white-point"></span>
+                            <span className="white-point"></span>
+                            <span className="white-point"></span> */}
+                            <FontAwesomeIcon icon={faEllipsisVertical} className="fa-2x"/>
+                        </div>
+                    </div>
+                </div>
                 
-                {/*  */}
+                {/* Navbar main block */}
                 <div className="navbar-main">
                     <ul className="navbar-menu">
                         <li>
@@ -49,6 +64,14 @@ const Navbar = () => {
                             </a>
                         </li>
                     </ul>
+                    <div className="navbar-search block-m">
+                        <form action="/tim-kiem" method="get">
+                            <input className="search-input" type="text" placeholder="Tối thiểu 2 ký tự" name="keywords"/>
+                            <button className="search-submit" type="submit" value={"Tìm kiếm"}>
+                                <FontAwesomeIcon icon={faMagnifyingGlass} className="font-black"/>
+                            </button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
